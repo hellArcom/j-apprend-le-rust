4f7c604017e6af092295a0ee9ebd9e3363c577f7
Arcom <argam7300@gmail.com>
2025-04-20 11:59:55 +0200

    modif

---
+++ 2 Variable/Variable.rs
@@ -0,0 +1,43 @@
+fn main() {
+      /////////////////////////////
+     //// Variable modifiable ////
+    ////////////////////////////
+    let mut age = 20; // si on mais pas "mut" la valeur ne peut pas être modifier
+    age = 21; // changer la vriable
+    println("Age : {}", age); // afficher la variable + texte
+
+    /////////////////////////////
+    // Variable non modifiable //
+    ////////////////////////////
+
+    let nom = "Arcom"; //non modifiable
+    println!("Nom : {}", nom); // afficher la variable + texte
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////TYPES DSE DONNER : ////////////////////////////////////////////////////////////////////////
+
+    ////////////////////////////////////////////
+    ///////Convertion de donner numérique //////
+    ////////////////////////////////////////////
+    
+    // /!\ Attention à :
+    // let x: u8 = 255;
+    // let y = x as i8;
+    // println!("{}", y); // Affiche : -1 
+    // i8 va de -128 à 127 donc 255 dépasse ce qui affiche -1 :/
+
+    let a: i32 = 10; // définir a en i32 (entier 32 bits)
+    let b: f64 = a as f64; // défnir b en tant que f64 a a changer en f64 (f64 = nombre a virgule 64 bitss)
+    println!("a en f64 : {}", b);
+
+    ///////////////////////////////
+    /////// De str a nombre ///////
+    ///////////////////////////////
+
+    let chain = "42";
+    let nombre: i32 = chain.parse().unwrap();
+    println!("Nombre : {]", nombre);
+
+    
+}
+
